e6c541bece37f4adf7174da7a595328f39be8762
frontend <gabriellennon@wizsolucoes.com.br>
2022-06-27 15:45:51 -0300

    install router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
-import { ApolloProvider } from '@apollo/client';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { client } from './lib/apollo';
 
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* Is a context provider of apollo */}
-    <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>
   </React.StrictMode>
 )
